844f6c4ca0d2b9e2263ea0c14c233b53a0be7987
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-04-20 12:08:27 +0530

    Time: 118 ms (52.00%), Space: 79.7 MB (43.27%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,32 +1,32 @@
 class Solution {
-public:
-int n, m;
-    bool inside(int i, int j) {
-        return (0<=i)&&(i<n)&&(0<=j)&&(j<m);
-    }
     vector<vector<int>> ans;
-    void dfs(int i, int j, int& r1, int& c1, int& r2, int& c2,vector<vector<int>>& land){
-        if (!inside(i, j)) return;
-        if (land[i][j]==1){
-            land[i][j]=2;
-            r1=min(r1, i), c1=min(c1, j), r2=max(r2, i), c2=max(c2, j);
-            dfs(i+1,j, r1, c1, r2, c2, land);
-            dfs(i,j+1, r1, c1, r2, c2, land);
-            dfs(i-1,j, r1, c1, r2, c2, land);
-            dfs(i,j-1, r1, c1, r2, c2, land);
-        }          
+private:
+    void dfs(int i, int j, int &r1, int &c1, int &r2, int &c2, vector<vector<int>> &land) {
+        int n = land.size();
+        int m = land[0].size();
+        if (i >= 0 && i < n && j >= 0 && j < m && land[i][j] == 1) { // Boundary check
+            land[i][j] = 2;
+            r1 = min(r1, i), c1 = min(c1, j), r2 = max(r2, i), c2 = max(c2, j);
+            dfs(i + 1, j, r1, c1, r2, c2, land); // Recursive call with boundary check
+            dfs(i, j + 1, r1, c1, r2, c2, land); // Recursive call with boundary check
+            dfs(i - 1, j, r1, c1, r2, c2, land); // Recursive call with boundary check
+            dfs(i, j - 1, r1, c1, r2, c2, land); // Recursive call with boundary check
+        }
     }
+public:
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        n=land.size();
-        m=land[0].size();
-        for(int i=0; i<n; i++)
-            for(int j=0; j<m; j++){
-                if(land[i][j]==1){
-                    int r1=i, c1=j, r2=i, c2=j;
+        int n = land.size();
+        int m = land[0].size();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (land[i][j] == 1) {
+                    int r1 = i, c1 = j, r2 = i, c2 = j;
                     dfs(i, j, r1, c1, r2, c2, land);
                     ans.push_back({r1, c1, r2, c2});   
                 }
             }
+        }
         return ans;
     }
 };
