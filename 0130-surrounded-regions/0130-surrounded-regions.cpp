1735953d487f23ececa6af8883be5006d19c1bf8
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-05-19 17:44:32 +0530

    Time: 7 ms (92.51%), Space: 13.9 MB (50.18%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,38 +1,39 @@
 class Solution {
 private:
-    void dfs(int row, int col, vector<vector<char>> &vis,vector<vector<char>> &board,int delrow[],int delcol[]){
+    void dfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>&board){
         vis[row][col] = 1;
         int n = board.size();
         int m = board[0].size();
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
         for(int i=0;i<4;i++){
-            int nrow = row+delrow[i];
-            int ncol = col+delcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && board[nrow][ncol]=='O'){
-                dfs(nrow,ncol,vis,board,delrow,delcol);
+            int r = row+dr[i];
+            int c = col+dc[i];
+            if(r>=0 && c>=0 && r<n && c<m && !vis[r][c] && board[r][c]=='O'){
+                dfs(r,c,vis,board);
             }
         }
+        
     }
 public:
     void solve(vector<vector<char>>& board) {
         int n = board.size();
         int m = board[0].size();
-        vector<vector<char>> vis(n,vector<char>(m,0));
-        int delrow[] = {-1,0,1,0};
-        int delcol[] = {0,1,0,-1};
+        vector<vector<int>>vis(n,vector<int>(m,0));
         for(int j=0;j<m;j++){
             if(board[0][j] == 'O'){
-                dfs(0,j,vis,board,delrow,delcol);
+                dfs(0,j,vis,board);
             }
             if(board[n-1][j]=='O'){
-                dfs(n-1,j,vis,board,delrow,delcol);
+                dfs(n-1,j,vis,board);
             }
         }
         for(int i=0;i<n;i++){
             if(board[i][0] == 'O'){
-                dfs(i,0,vis,board,delrow,delcol);
+                dfs(i,0,vis,board);
             }
             if(board[i][m-1]=='O'){
-                dfs(i,m-1,vis,board,delrow,delcol);
+                dfs(i,m-1,vis,board);
             }
         }
         for(int i=0;i<n;i++){
@@ -42,5 +43,6 @@ public:
                 }
             }
         }
+        
     }
 };
