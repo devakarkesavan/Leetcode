8b002b95472804cf7ebcdd502d9f2b774fae36b7
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-05-20 13:08:22 +0530

    Time: 147 ms (36.29%), Space: 62.5 MB (46.02%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -22,21 +22,55 @@ private:
     }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n= graph.size();
-        vector<int>vis(n,0);
-        vector<int>pathVis(n,0);
-        vector<int>check(n,0);
+        // int n= graph.size();
+        // vector<int>vis(n,0);
+        // vector<int>pathVis(n,0);
+        // vector<int>check(n,0);
+        // for(int i=0;i<n;i++){
+        //     if(!vis[i]){
+        //         dfs(i,vis,pathVis,check,graph);
+        //     }
+        // }
+        // vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     if(check[i]==1){
+        //         ans.push_back(i);
+        //     }
+        // }
+        // return ans;
+        int n = graph.size();
+        vector<int>adjRev[n];
+        
+        vector<int>indegree(n,0);
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,vis,pathVis,check,graph);
+            for(auto it:graph[i]){
+                adjRev[it].push_back(i);
+                indegree[i]++;
             }
         }
-        vector<int>ans;
+        
+        queue<int>q;
         for(int i=0;i<n;i++){
-            if(check[i]==1){
-                ans.push_back(i);
+            if(indegree[i]==0){
+                q.push(i);
             }
         }
-        return ans;
+        
+        vector<int>safeNodes;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            safeNodes.push_back(node);
+            
+            for(auto it:adjRev[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        sort(safeNodes.begin(),safeNodes.end());
+        return safeNodes;
+        
     }
 };
