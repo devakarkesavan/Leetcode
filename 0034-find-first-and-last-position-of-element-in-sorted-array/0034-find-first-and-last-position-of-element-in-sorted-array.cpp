358475b272035dd8b948bd9ef2ba4976813d3a90
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-06-01 14:24:27 +0530

    Time: 3 ms (88.94%), Space: 16 MB (86.18%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    int upperBound(vector<int>&nums,int target){
+    int upperBound(vector<int>& nums, int target) {
         int n = nums.size();
-        int low = 0,high = n-1;
-        int ans= -1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid]<target){
+        int low = 0, high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > target) {
                 ans = mid;
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return ans;
     }
-public:
-    int lowerBound(vector<int>&nums,int target){
+
+    int lowerBound(vector<int>& nums, int target) {
         int n = nums.size();
-        int low = 0,high = n-1;
-        int ans= -1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid]>=target){
+        int low = 0, high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] >= target) {
                 ans = mid;
-                high= mid-1;
-            }
-            else{
-                low = mid+1;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return ans;
     }
-public:
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(find(nums.begin(), nums.end(), target)==nums.end()) return {-1, -1};
-        int k = lower_bound(nums.begin(), nums.end(),target) - nums.begin();
-        int p = upper_bound(nums.begin(), nums.end(),target) - nums.begin();
-        return {k, p-1};
+        int k = lowerBound(nums, target);
+        if (k == nums.size() || nums[k] != target) {
+            return {-1, -1};
+        }
+        int p = upperBound(nums, target);
+        return {k, p - 1};
     }
 };
