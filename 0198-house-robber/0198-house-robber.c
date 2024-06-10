035cdb76ba8c14a2e6caa2cdd6bbf5709f3f2c66
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-06-10 18:46:14 +0530

    Time: 3 ms (44.31%), Space: 5.7 MB (36.65%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.c
@@ -0,0 +1,26 @@
+int max(int a,int b){
+    return (a>b) ? a : b;
+}
+int fun(int ind,int* dp,int* nums){
+    if(ind==0){
+        return nums[0];
+    }
+    if(ind<0){
+        return 0;
+    }
+    if(dp[ind]!=-1){
+        return dp[ind];
+    }
+    int nt = fun(ind-1,dp,nums);
+    int take = nums[ind]+fun(ind-2,dp,nums);
+    return dp[ind] = max(nt,take);
+}
+int rob(int* nums, int n) {
+    int *dp = (int*)malloc((n+1)*sizeof(int));
+    for(int i=0;i<=n;i++){
+        dp[i] = -1;
+    }
+    return fun(n-1,dp,nums);
+    free(dp);
+    // return result;
+}
