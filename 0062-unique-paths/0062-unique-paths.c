c807a62df18b0d2f2032b334e87d45a5ff4bd760
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-06-10 20:22:02 +0530

    Time: 3 ms (18.73%), Space: 5.6 MB (16.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.c
+++ 0062-unique-paths/0062-unique-paths.c
@@ -18,10 +18,30 @@ int uniquePaths(int m, int n) {
     for(int i=0;i<m;i++){
         dp[i] = (int*)malloc(n*sizeof(int));
         for(int j=0;j<n;j++){
-            dp[i][j] = -1;
+            dp[i][j] = 0;
         }
     }
-    int result =  fun(m-1,n-1,dp);
-    free(dp);
-    return result;
+    // int result =  fun(m-1,n-1,dp);
+    // free(dp);
+    // return result;
+    dp[0][0] = 1;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 && j==0){
+                dp[0][0] = 1;
+                continue;
+            }
+            else{
+            int left=0,up = 0;
+            if(j-1>=0){
+             left = dp[i][j-1];
+            }
+            if(i-1>=0){
+             up = dp[i-1][j];
+            }
+            dp[i][j] = left+up;
+            }
+        }
+    }
+    return dp[m-1][n-1];
 }
