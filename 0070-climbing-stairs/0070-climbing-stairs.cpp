c56a61cf256360ab512a06820b8a3090d1d95706
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-06-10 14:04:26 +0530

    Time: 3 ms (29.59%), Space: 7.5 MB (11.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -16,11 +16,14 @@ public:
             return 1;
         }
         vector<long long int>dp(n+1,-1);
-        dp[0] = 1;
-        dp[1] = 1;
+        long long int prev = 1;
+        long long int prev2 = 1;
+        long long int curr;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            curr = prev+prev2;
+            prev2 = prev;
+            prev = curr;
         }
-        return (int)dp[n];
+        return (int)prev;
     }
 };
