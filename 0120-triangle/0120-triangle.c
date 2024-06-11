5fc02e1c19dcd3f5d7f45364a0bc5824b92d6a56
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-06-11 09:13:07 +0530

    Time: 3 ms (92.78%), Space: 7 MB (5.67%) - LeetHub

--- 0120-triangle/0120-triangle.c
+++ 0120-triangle/0120-triangle.c
@@ -14,24 +14,33 @@ int fun(int i,int j,int** dp, int** arr,int n){
 }
 int minimumTotal(int** triangle, int triangleSize, int* triangleColSize) {
     int n = triangleSize;
-    int **dp = (int**)malloc(n*sizeof(int*));
-    for(int i=0;i<n;i++){
-        dp[i] = (int*)malloc(n*sizeof(int));
-        for(int j=0;j<n;j++){
-            dp[i][j] = 0;
-        }
+    // int **dp = (int**)malloc(n*sizeof(int*));
+    // for(int i=0;i<n;i++){
+    //     dp[i] = (int*)malloc(n*sizeof(int));
+    //     for(int j=0;j<n;j++){
+    //         dp[i][j] = 0;
+    //     }
+    // }
+    int *next =  (int*)malloc(n*sizeof(int));
+    for(int j=0;j<n;j++){
+         next[j] = 0;
     }
     for(int i=0;i<n;i++){
-        dp[n-1][i] = triangle[n-1][i];
+        next[i] = triangle[n-1][i];
     }
     for(int i=n-2;i>=0;i--){
-        for(int j=i;j>=0;j--){
-            int d1 = dp[i+1][j] + triangle[i][j];
-            int d2 = dp[i+1][j+1] + triangle[i][j];
-            dp[i][j]= min(d1,d2);
+        int *curr =  (int*)malloc(n*sizeof(int));
+        for(int j=0;j<n;j++){
+             curr[j] = 0;
         }
+        for(int j=i;j>=0;j--){
+            int d1 = next[j] + triangle[i][j];
+            int d2 = next[j+1] + triangle[i][j];
+            curr[j]= min(d1,d2);
+        }
+        next = curr;
     }
-    return dp[0][0];
+    return next[0];
     // int result = fun(0,0,dp,triangle,n);
     // for(int i=0;i<n;i++){
     //     free(dp[i]);
