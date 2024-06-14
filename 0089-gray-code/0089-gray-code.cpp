0cf08c4b35b4b0e60281c4a5650df012b0f668dc
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-06-14 19:55:20 +0530

    Time: 3 ms (84.83%), Space: 13.6 MB (20.87%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> vect;
+    void helper(int &a,int n)
+    {
+        a^=1<<n;
+        vect.push_back(a);
+        for(int c=0;c<n;c++)
+            helper(a,c);
+    }
+    vector<int> grayCode(int n) 
+    {
+        int a=0;
+        vect.push_back(a);
+        for(int c=0;c<n;c++)
+            helper(a,c);
+        return vect;  
+    }
+};
