b4eed74b5ff598b72b033b403249bf85bf9d5413
Devakar <103299997+devakarkesavan@users.noreply.github.com>
2024-06-16 21:53:18 +0530

    Time: 0 ms (100.00%), Space: 13.7 MB (82.46%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+         long long patch = 0;
+    int count = 0;
+    int index = 0;
+    while (patch < n) {
+        if (index < nums.size() && patch + 1 >= nums[index]) {
+            patch += nums[index];
+            index++;
+        } else {
+            patch += (patch + 1);
+            count++;
+        }
+    }
+
+    return count;
+    }
+};
